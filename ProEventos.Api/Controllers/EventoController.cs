a89bce863f439fd108a763da3256ea231405a1fb
Andre-luiz <andretm4@hotmail.com>
2022-05-01 22:13:50 -0300

    projeto evento api inicializado

---
+++ ProEventos.Api/Controllers/EventoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.Api.Models;
+
+namespace ProEventos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+  
+
+        private readonly ILogger<EventoController> _logger;
+
+        public EventoController(ILogger<EventoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento()
+            {
+                EventoId = 1,
+                Tema = "angular e net 5",
+                Local = "São carlos",
+                Lote = "1 lote",
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                QtdPessoas = 250,
+                ImageUrl = "foto.png"
+            };
+        }
+    }
+}
